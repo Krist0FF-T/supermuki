a9cca59fa1f43f983e97b0250799675a425eebdc
Krist0FF-T <155083075+Krist0FF-T@users.noreply.github.com>
2025-08-10 20:13:25 +0200

    asset manager: actually lazy-load

--- scripts/assets.py
+++ scripts/assets.py
@@ -1,94 +1,63 @@
 import pygame as pg
-from .consts import *
-# from pygame.image import load
-# from pygame.transform import scale
+from . import consts
 
-pg.mixer.init()
-pg.display.init()
-pg.font.init()
 
 class AssetManager:
     def __init__(self):
-        self.loaded = {}
+        pg.mixer.init()
+        pg.display.init()
+        pg.font.init()
 
+        self.images: dict[str, pg.Surface] = {}
+        self.sounds: dict[str, pg.Sound] = {}
+        self.font_cache: dict[int, pg.Font] = {}
+        self.player_images: list[pg.Surface] = []
 
-    def get_sound(self, name):
-        path = f"assets/sound/{name}"
+        self.audio_volume = 1.0
 
-        if path in self.loaded:
-            return self.loaded[path]
+    def play_sound(self, name: str):
+        sound = self.get_sound(name)
+        sound.set_volume(self.audio_volume)
+        sound.play()
 
-        return pg.mixer.Sound(path)
+    def get_sound(self, name: str) -> pg.Sound:
+        sound = self.sounds.get(name)
 
+        if sound is None:
+            path = f"assets/sound/{name}"
+            print("loading sound", name)
+            sound = pg.mixer.Sound(path)
+            self.sounds[name] = sound
 
-    def get_image(self, name):
-        path = f"assets/img/{name}"
+        return sound
 
-        if path in self.loaded:
-            return self.loaded[path]
+    def get_image(self, name: str) -> pg.Surface:
+        img = self.images.get(name)
+        if img is None:
+            path = f"assets/img/{name}"
+            img = pg.image.load(path)
+            print("loading image", name)
+            self.images[name] = img
+        return img
 
-        return pg.image.load(path)
+    def get_font(self, size: int = 20) -> pg.Font:
+        font = self.font_cache.get(size)
+        if font is None:
+            print("creating font", size)
+            font = pg.Font(consts.FONT, size)
+            self.font_cache[size] = font
+        return font
 
-
-    def get_font(self, size: int=20, name=FONT_NAME):
-        return pg.Font(f"assets/fonts/{name}", size)
-
-
-    def load_player_imgs(self, path="player"):
-        self.player_images = []
+    def load_player_imgs(self):
+        self.player_images.clear()
         for i in range(1, 4):
-            img = self.get_image(f"{path}/{i}.png").convert()
+            img = self.get_image(f"player/{i}.png").convert()
             img.set_colorkey((0, 0, 0))
             img = pg.transform.scale(img, (27, 27))
             self.player_images.append(img)
 
+    def set_audio_volume(self, f: float):
+        self.audio_volume = f
 
 
 asset_manager = AssetManager()
-
-# # sounds
-# load_sound = lambda name: pg.mixer.Sound(f"assets/sound/{name}")
-# trampoline_fx = load_sound("trampoline.wav")
-# trampoline_fx.set_volume(0.2)
-# completed_fx = load_sound("level_completed.wav")
-# completed_fx.set_volume(0.4)
-#
-# enemy_hit_fx = load_sound("enemy_hit.wav")
-#
-# checkpoint_fx = load_sound("checkpoint.wav")
-# shoot_fx = load_sound("shoot.wav")
-# laser_fx = load_sound("laser.wav")
-#
-# # images
-#
-# player_images = []
-# for i in range(1, 4):
-#     img = pg.image.load(f'assets/img/player/{i}.png').convert()
-#     img.set_colorkey((0, 0, 0))
-#     img = pg.transform.scale(img, (27, 27))
-#     player_images.append(img)
-#
-# enemy_images = [
-#     scale(load(f'assets/img/blocks/enemy/{i}.png'), (TS, TS))
-#     for i in [1, 2]
-# ]
-#
-# _checkpoint_f = 'assets/img/blocks/checkpoint'
-# checkpoint_images = dict(
-#     active = scale(load(f'{_checkpoint_f}/active.png'),   (TS, TS)),
-#     inactive = scale(load(f'{_checkpoint_f}/inactive.png'), (TS, TS)),
-# )
-#
-# def gen_b_imgs(names: list[str]):
-#     return {
-#         name: load(f'assets/img/blocks/{name}.png')
-#         for name in names
-#     }
-#
-# b_imgs = gen_b_imgs([
-#     'enemy', 'grass', 'dirt', 'trampoline', 'moving', 'box', 'flag', 'shooter',
-#     'falling', 'spike', 'checkpoint', 'aimbot'
-# ])
-
-
-
